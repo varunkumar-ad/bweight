737d60eb525a5a0a3c0381afefa85c91aed0d93b
varunkumar-ad <144425724+varunkumar-ad@users.noreply.github.com>
2023-09-10 07:06:44 +0530

    main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def Pinfo():
         st.plotly_chart(fig1)
         t1 = df_selection.groupby(['sex'])['bweight'].aggregate(['sum', 'mean', 'median', 'max', 'min'])
         st.dataframe(t1)
-        st.write("1. Majority of birth weight lies between 2300 and 3900, but birth weight more than 4600 could be found in less proportion.")
-        st.write("2. Almost constant birth weight spread from 0 to 2100")
-        st.write("3. This graph shows many number of male newborns with birth weights are more compared to female newborns")
-
+        st.write("1. Majority of the newborn's birth weight lies between 2500 and 3700, but birth weight more than 4600 could be found in less proportion.")
+        st.write("2. Almost constant number of newborns from the birth weight spread between 0 to 2100")
+        st.write("3. From the table, first half of the male newborns are below the birth weight of 3290 ")
+        st.write("3. From the table, first half of the femmale newborns are below the birth weight of 3120 ")
     with tab2:
         matage_selection = st.slider('Mother age:',
                                   min_value=min(df['matage']),
@@ -42,26 +42,23 @@ def Pinfo():
         fig2 = px.scatter(df_selection[mask], x="matage", y="bweight", title="Birth weight based on mother's age",
                           labels={'bweight': 'Birth weight', 'matage': 'Mother age'}, color='sex')
         st.plotly_chart(fig2)
-        t2 = df_selection[mask].groupby(['sex'])['bweight'].aggregate(['sum', 'mean', 'median', 'max', 'min'])
-        st.dataframe(t2)
 
         mask = (df['matage'].between(*matage_selection)) & (df['sex'].isin(df_selection))
 
         st.write("1. From this graph we could see that more number of male newborns are greater than birth weight of 4000 compared to female newborns.")
         st.write("2. Also from this graph we could see that there are few number of newborns less than birth weight of 1000, in which majority are female newborns.")
-        st.write("3. Most of the newborn's birth weight is in the range of 2500 to 3900.")
-        st.write("4. From this graphs we could see that birth weight between 2500 to 4000 the mother's age is below 25 and the gender of the newborns are female.")
-        st.write("5. More birth weight spread in the range 2500 to 3700.")
+        st.write("3. Most of the newborn's birth weight is in the range of 2500 to 3700.")
+        st.write("4. From this graph we could see that birth weight between 2500 to 4000, the mother's age is below 25 and the gender of the newborns are female.")
 def Minfo():
-    options = st.selectbox('select the variable', ["Hypertension","Gestational week","Menstrual gap","Gestational age categories"])
+    options = st.selectbox('select the variable', ["Hypertension","Gestational week","Matagegp","Gestational age categories"])
     if options=='Hypertension':
         ht()
     elif options =='Gestational week':
         gestwks()
         st.write("1. From this graph we can see there are some newborns birth weight less than 1000 when the gestational week in the range between 24 to 32.")
-        st.write("2. Also from the graph we can see that maximum number of newborns birthweight in the range 2500 to 4000 and when the Gestational age is between 37 and 42.")
+        st.write("2. Also from the graph we can see that maximum number of newborns birthweight in the range 2500 to 4000 and when the Gestational week is between 37 and 42.")
         st.write("3. It is a postive slope line, from this we can say when birthweight depends on Gestational week")
-    elif options =='Menstrual gap':
+    elif options =='Matagegp ':
         matagegp()
     elif options =='Gestational age categories':
         gestcat()
@@ -72,7 +69,7 @@ def report():
     st.title('Conclusion')
     st.write('1. Birth weight of newborns are more consistent when the mother has no hypertension.')
     st.write('2. When the gestational week increases the birth weight of the newborns also increase respectively.')
-    st.write('3. Menstrual gap does not affect the birth weight.')
+    st.write('3. Matagegp does not affect the birth weight.')
     st.write('4. When the gestational age category 2, newborns birth weight is more consistent.')
     st.write('5. Average number of healthly newborn weight is in the range between 2500 to 4000 based on the graph.')
     st.write('6. Factor that affect birth weight are hypertension, sex, gestational week and gestational age categories.')
@@ -88,25 +85,27 @@ def ht():
     st.plotly_chart(fig1)
     t4 = df_selection.groupby(['ht'])['bweight'].aggregate(['sum', 'mean', 'median', 'max', 'min'])
     st.dataframe(t4)
-    st.write("1. From the graph person without hyper tension have more consistent birth weight compared to person with hypertension graph.")
+    st.write("1. From the graph person without hypertension have more consistent birth weight compared to person with hypertension graph.")
     st.write("2. Newborn with lowest birth weight is born for person with no hypertension.")
+    st.writr("3. The average birth weight of the person with hypertension is significantly less ")
 def gestwks():
     st.header('Gestational week')
     fig2 = px.scatter(df, x='gestwks', y='bweight', labels={'gestwks':'Gestational week','bweight':'Birth weight'})
     st.plotly_chart(fig2)
 def matagegp():
-    st.header('Menstrual gap')
-    matagegp_level = st.multiselect("Select the Menstrual gap:",
+    st.header('Matagegp ')
+    matagegp_level = st.multiselect("Select the Matagegp :",
                                    options=df["matagegp"].unique(),
                                    default=df["matagegp"].unique(), key='k5')
     df_selection = df.query(
         "matagegp == @matagegp_level"
     )
-    fig3 = px.box(df_selection, x='matagegp', y='bweight', labels={'matagegp':'Menstrual gap (in years)','bweight':'Birth weight'})
+    fig3 = px.box(df_selection, x='matagegp', y='bweight', labels={'matagegp':'Matagegp (in years)','bweight':'Birth weight'})
     st.plotly_chart(fig3)
     t5 = df_selection.groupby(['matagegp'])['bweight'].aggregate(['sum', 'mean', 'median', 'max', 'min'])
     st.dataframe(t5)
-    st.write("1. From this graph Menstrual gap does not affect the birth weight of the newborns.")
+    st.write("1. From this graph Matagegp does not affect the birth weight of the newborns.")
+    st.write('2. Maximum birth weight is significantly less for Matagegp ')
 def gestcat():
     st.header('Gestational age category')
     gestcat_level = st.multiselect("Select the gestational age category:",
@@ -131,7 +130,7 @@ elif side == 'Data Header':
     Data_Header()
     st.write('Dimensions of the dataset')
     df.shape
-    st.write("Attributes \n1) Matage - Mother's age \n2) Ht - Hypertension, whether the mother is hypertension patient or not. \n3) Gestwks - Gestational age is the common term used during pregnancy to describe how far along the pregnancy is. It is measured in weeks. \n4) Bweight - Birth weight is the first weight of the newborn baby, taken just after being born. It is measured in grams (g). \n5) Matagegp - Mother's gap between the first baby and next baby (in years).  \n6) Gestcat - Gestational age categories.")
+    st.write("Attributes \n1) Matage - Mother's age \n2) Ht - Hypertension, whether the mother is hypertension patient or not. \n3) Gestwks - Gestational age is the common term used during pregnancy to describe how far along the pregnancy is. It is measured in weeks. \n4) Bweight - Birth weight is the first weight of the newborn baby, taken just after being born. It is measured in grams (g). \n5) Matagegp - Mother's gap between the first baby and next baby .  \n6) Gestcat - Gestational age categories.")
 
 elif side =='Personal Info':
     Pinfo()
